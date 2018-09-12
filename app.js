9134990a5ac55c50f6d61514f0dc80da1af177b7
Amit Kulhari <amitkulhari26@gmail.com>
2018-09-12 22:41:24 +0530

    first working commit

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ var bodyParser=require('body-parser');
 var exphbs=require('express-handlebars');
 
 var app=express();
-var port=process.env.PORT || 1000;
 
+// Handlebars middleware
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine','handlebars');
+
+// Body Parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Set Static folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index route
+app.get('/',function (req, res) {
+  res.render('index');
+});
+
+var port=process.env.PORT || 1000;
 app.listen(port,function(){
   console.log('Magic at 1000');
 });
