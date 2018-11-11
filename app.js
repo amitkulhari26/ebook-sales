0dad57ab7e52602c8453d358057e8fc0e1bef5a0
amitkulhari26 <amitkulhari26@gmail.com>
2018-11-12 00:22:54 +0530

    first flow is developed working fine

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
-var express=require('express');
-var stripe=require('stripe')('sk_test_go4mUAcLt2dXxMKK6X19VEIf');
-var bodyParser=require('body-parser');
-var exphbs=require('express-handlebars');
+var express = require('express');
+var stripe = require('stripe')('sk_test_go4mUAcLt2dXxMKK6X19VEIf');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
 
-var app=express();
+var app = express();
 
 // Handlebars middleware
-app.engine('handlebars',exphbs({defaultLayout:'main'}));
-app.set('view engine','handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // Body Parser middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set Static folder
 app.use(express.static(`${__dirname}/public`));
 
 // Index route
-app.get('/',function (req, res) {
+app.get('/', function (req, res) {
   res.render('index');
 });
 
-var port=process.env.PORT || 1000;
-app.listen(port,function(){
-  console.log('Magic at 1000');
+// charge route
+
+app.post('/charge', function (req, res) {
+  var amount = 2500;
+
+  console.log('credit card data', req.body);
+
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer => stripe.charges.create({
+    amount: amount,
+    description: 'Web Development Ebook',
+    currency: 'usd',
+    customer: customer.id
+  }))
+    .then(charge => res.render('sucess'));
+})
+
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log('Magic at 5000');
 });
